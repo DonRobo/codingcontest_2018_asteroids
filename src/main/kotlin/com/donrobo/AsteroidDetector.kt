69699adec74b6fcf314f7b0cad326ca59b1a0c35
Robert Tieber <robert.tieber@student.tugraz.at>
2018-04-27 17:41:39 +0200

    Fixed loops to get correct result for lvl4 again

--- src/main/kotlin/com/donrobo/AsteroidDetector.kt
+++ src/main/kotlin/com/donrobo/AsteroidDetector.kt
@@ -28,8 +28,11 @@ fun detectAsteroids(input: Input): Output {
                 if ((input.endObservation - t.timestamp) / period > unaccountedFor.size) continue
 
                 val expected = (t.timestamp..input.endObservation step period).map { it }
-                if (unaccountedFor.map { it.timestamp }.containsAll(expected)) {
-                    for (rotatingBy in 0..3) {
+                for (rotatingBy in 0..3) {
+                    val offset = t.timestamp - input.startObservation
+                    if (offset > period - 1) continue
+
+                    if (unaccountedFor.map { it.timestamp }.containsAll(expected)) {
                         var currentRotation = t
                         var ok = true
                         unaccountedFor.filter { it.timestamp in expected }.sortedBy { it.timestamp }.forEach {
