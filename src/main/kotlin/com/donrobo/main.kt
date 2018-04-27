974481c1d459e8ee8e15d329989e575684e78487
Robert Tieber <robert.tieber@student.tugraz.at>
2018-04-27 15:57:39 +0200

    Prepared for level 3

--- src/main/kotlin/com/donrobo/main.kt
+++ src/main/kotlin/com/donrobo/main.kt
@@ -3,14 +3,14 @@ package com.donrobo
 import java.io.File
 
 fun main(args: Array<String>) {
-    val inputs = File("src/main/resources/input2")
+    val inputs = File("src/main/resources/input3")
     inputs.listFiles().forEach { inputFile ->
         val output = detectAsteroids(parseInput(inputFile.readText()))
         val resultOutput = output.resultOutput()
 
         println(inputFile.name)
         println(resultOutput)
-        File(File("output2"), inputFile.name).writeText(resultOutput)
+        File(File("output3"), inputFile.name).writeText(resultOutput)
     }
 }
 
